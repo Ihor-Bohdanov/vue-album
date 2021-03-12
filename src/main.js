0749ca4f30af4ba4ff67b2e05a24f7a4ea194245
Ihor Bohdanov <gosha0990dnepr@gmail.com>
2021-03-12 21:40:47 +0200

    add ant design

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
+import "ant-design-vue/dist/antd.css";
+import "@/assets/scss/main.scss";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
