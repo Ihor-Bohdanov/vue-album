f6cb88695cbd3acaeb9c6f32513c3064e7a2dc1d
Ihor Bohdanov <gosha0990dnepr@gmail.com>
2021-03-15 22:11:59 +0200

    add main pages

---
+++ src/views/pages/Albums.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page">
+    <h1 class="page__title">Albums</h1>
+    <AlbumsList :albums="albums" />
+  </div>
+</template>
+
+<script>
+import AlbumsList from "@/components/albums/AlbumsList";
+export default {
+  name: "Albums",
+  computed: {
+    albums() {
+      return this.$store.state.albums;
+    }
+  },
+  components: {
+    AlbumsList
+  }
+};
+</script>
---
+++ src/views/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page">
+    <h1 class="page__title">Home</h1>
+    <ol>
+      <li>
+        Реализовать небольшое CRUD Web приложение используя фреймворк на выбор
+        (React/Angular/Vue)
+      </li>
+      <li>
+        В качестве бэкэнда использовать сервис
+        http://jsonplaceholder.typicode.com/
+      </li>
+      <li>
+        Вывести список альбомов используя API GET
+        http://jsonplaceholder.typicode.com/albums
+      </li>
+      <li>
+        Вывести список фото для каждого альбома, используя роутинг и API GET
+        http://jsonplaceholder.typicode.com/albums/1/photos где 1 - id альбома
+      </li>
+      <li>
+        Реазизовать добавление/изменение/удаление альбомов, используя API
+        POST/PUT/DELETE http://jsonplaceholder.typicode.com/albums/1
+      </li>
+      <li>Стилизация приложения будет плюсом</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
