380d66c1bee98d607042a45b481989a85402fb0b
Ihor Bohdanov <gosha0990dnepr@gmail.com>
2021-03-15 22:13:43 +0200

    add 404

---
+++ src/views/pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-result status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
+    <template #extra>
+      <a-button type="primary">
+        Back Home
+      </a-button>
+    </template>
+  </a-result>
+</template>
+
+<script>
+import { Button, Result } from 'ant-design-vue'
+export default {
+  name: "404",
+  components: {
+    "a-result": Result,
+    "a-button": Button
+  }
+}
+</script>
+
+<style>
+
+</style>
